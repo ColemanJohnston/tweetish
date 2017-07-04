85be090b1dd03c56d6fdd86707efacfbe3c2df12
Coleman Johnston <colemanmav@fb.com>
2017-07-03 17:49:37 -0700

    Changed title bar to display screen name in profile view instead of app name.

--- app/src/main/java/com/codepath/apps/tweetish/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/tweetish/ProfileActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import com.codepath.apps.tweetish.models.User;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 import fragments.UserTimelineFragment;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TwitterClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +32,19 @@ public class ProfileActivity extends AppCompatActivity {
 
         ft.commit();
 
+        client = TwitterApp.getRestClient();
+        client.getUserInfo(new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    User user = User.fromJSON(response);
+                    getSupportActionBar().setTitle("@" + user.screenName);
+                }
+                catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
